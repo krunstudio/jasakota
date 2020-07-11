59dc2f97b477cbd56c57a460ce79804fd7b79dad
krunstudio <sinaskhairunnas@gmail.com>
2020-07-11 17:28:02 +0700

    setup deploy

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import store from './store';
 
