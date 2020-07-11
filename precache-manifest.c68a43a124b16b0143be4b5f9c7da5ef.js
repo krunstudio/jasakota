dd44fbf54d268f3b6672d90ba7c66286873051c1
krunstudio <sinaskhairunnas@gmail.com>
2020-07-11 17:17:30 +0700

    Updates

---
+++ precache-manifest.c68a43a124b16b0143be4b5f9c7da5ef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4691a5205f637b6e1e8811863642d15d",
+    "url": "/jasakota/index.html"
+  },
+  {
+    "revision": "eefc1a595ef6c8739ede",
+    "url": "/jasakota/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f2f167c15c179b4d9d96",
+    "url": "/jasakota/static/css/main.8716abab.chunk.css"
+  },
+  {
+    "revision": "eefc1a595ef6c8739ede",
+    "url": "/jasakota/static/js/2.7b7694f3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jasakota/static/js/2.7b7694f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2f167c15c179b4d9d96",
+    "url": "/jasakota/static/js/main.ccbfe7bd.chunk.js"
+  },
+  {
+    "revision": "2880404ea804eabac190",
+    "url": "/jasakota/static/js/runtime-main.e5761a7b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jasakota/static/media/logo.5d5d9eef.svg"
+  }
+]);
