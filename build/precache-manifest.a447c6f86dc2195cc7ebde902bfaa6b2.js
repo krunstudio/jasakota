4392d79821bcae56a7d31bfdace2cc6a40e2046c
krunstudio <sinaskhairunnas@gmail.com>
2020-08-18 23:18:52 +0700

    package json

---
+++ build/precache-manifest.a447c6f86dc2195cc7ebde902bfaa6b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "838f8b78c503337d70100fa271de3d76",
+    "url": "/jasakota/index.html"
+  },
+  {
+    "revision": "9b1c7c020092537f7e24",
+    "url": "/jasakota/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "813a776269b4b79b3e12",
+    "url": "/jasakota/static/css/main.79a21aa6.chunk.css"
+  },
+  {
+    "revision": "9b1c7c020092537f7e24",
+    "url": "/jasakota/static/js/2.fb679f2f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jasakota/static/js/2.fb679f2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "813a776269b4b79b3e12",
+    "url": "/jasakota/static/js/main.3e1cd348.chunk.js"
+  },
+  {
+    "revision": "2880404ea804eabac190",
+    "url": "/jasakota/static/js/runtime-main.e5761a7b.js"
+  }
+]);
